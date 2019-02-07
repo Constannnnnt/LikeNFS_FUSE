1e62d759dbf6ddc6b61d7a1d854fb9eb9735bc22
Yuan Chen <constantchen525@gmail.com>
2019-02-07 22:59:56 +0000

    update: init fuse

--- nfsFuseClient.cc
+++ nfsFuseClient.cc
@@ -1,32 +1,75 @@
+/*
+ * Fuse Client, implemented based on FUSE: Filesystem in Userspace
+ */
 #define FUSE_USE_VERSION 31
 
 #include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <assert.h>
 
-#include <grpc/grpc.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/create_channel.h>
-#include <grpcpp/security/credentials.h>
-#include "nfsFuse.grpc.pb.h"
 #include "nfsFuseGrpcClient.h"
 
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::ClientReader;
-using grpc::ClientReaderWriter;
-using grpc::ClientWriter;
-using grpc::Status;
-using nfsFuse::NFSFuse;
-using nfsFuse::GetAttrRequestParams;
-using nfsFuse::GetAttrResponseParams;
-using nfsFuse::ReadDirRequestParams;
-using nfsFuse::ReadDirResponseParams;
 
-int main(int argc, char** argv) {
-    /*
-     * Specify a mount point through arguments, wait to implement
-     * The third argument of client should be the mount point
-     */ 
+/*
+ * Command line options
+ *
+ * We can't set default values for the char* fields here because
+ * fuse_opt_parse would attempt to free() them when the user specifies
+ * different values on the command line.
+ */
+static struct options {
+    nfsFuseGrpcClient* nfsFuseClient;
+    int show_help;
+} options;
 
-  return 0;
+#define OPTION(t, p)                           \
+    { t, offsetof(struct options, p), 1 }
+static const struct fuse_opt option_spec[] = {
+    OPTION("-h", show_help),
+    OPTION("--help", show_help),
+    FUSE_OPT_END
+};
+
+static void show_help(const char *progname)
+{
+    std::cout << ("usage: %s [options] <mountpoint>\n\n", progname);
+}
+
+/*
+ * Define FUSE operations 
+ */
+static struct fuse_operations client_oper = {
+    
+};
+
+int main(int argc, char* argv[]) {
+    int ret;
+    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+    options.nfsFuseClient = new nfsFuseGrpcClient(grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials()));
+
+    /* Parse options */
+    if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1) {
+        return 1;
+    }
+
+    /* When --help is specified, first print our own file-system 
+     * specific help text, then signal fuse_main to show 
+     * additional help (by adding `--help` to the options again) 
+     * without usage: line (by setting argv[0] to the empty 
+     * string)
+     */
+    if (options.show_help) {
+        show_help(argv[0]);
+        assert(fuse_opt_add_arg(&args, "--help") == 0);
+        args.argv[0][0] = '\0';
+    }
+
+    ret = fuse_main(args.argc, args.argv, &client_oper, NULL);
+    fuse_opt_free_args(&args);
+    return ret;
 }
